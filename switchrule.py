d7ff5c0cd6c6858762cb4a222addfdbcbf8da5b2
max2max <2206460474@qq.com>
2018-01-29 17:36:11 +0800

    Update switchrule.py

--- switchrule.py
+++ switchrule.py
@@ -2,19 +2,13 @@ from configloader import load_config, get_config
 
 
 def getKeys():
-    key_list = ['id', 'port', 'u', 'd', 'transfer_enable', 'passwd', 'enable']
+    key_list = ['id', 'port', 'u', 'd', 'transfer_enable', 'enable']
     if get_config().API_INTERFACE == 'sspanelv3':
         key_list += ['method']
     elif get_config().API_INTERFACE == 'sspanelv3ssr':
         key_list += ['method', 'obfs', 'protocol']
     elif get_config().API_INTERFACE == 'glzjinmod':
-        key_list += ['method',
-                     'obfs',
-                     'obfs_param',
-                     'protocol',
-                     'protocol_param',
-                     'id',
-                     'node_speedlimit',
+        key_list += ['node_speedlimit',
                      'forbidden_ip',
                      'forbidden_port',
                      'disconnect_ip',
