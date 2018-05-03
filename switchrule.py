022baf9ccda529eb13fe67381999cd58038d90e0
max2max <14960653+max2max@users.noreply.github.com>
2018-05-03 21:15:11 +0800

    Update switchrule.py

--- switchrule.py
+++ switchrule.py
@@ -8,17 +8,11 @@ def getKeys():
 	elif get_config().API_INTERFACE == 'sspanelv3ssr':
 		key_list += ['method', 'obfs', 'protocol']
 	elif get_config().API_INTERFACE == 'glzjinmod':
-		key_list += ['method',
-					 'obfs',
-					 'obfs_param',
-					 'protocol',
-					 'protocol_param',
-					 'id',
-					 'node_speedlimit',
-					 'forbidden_ip',
-					 'forbidden_port',
-					 'disconnect_ip',
-					 'is_multi_user']
+		key_list += ['node_speedlimit',
+				'forbidden_ip',
+				'forbidden_port',
+				'disconnect_ip',
+				'is_multi_user']
 	return key_list
 	# return key_list + ['plan'] # append the column name 'plan'
 
